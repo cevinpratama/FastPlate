35e62db42f7dfcfb8d1647c68e92e27948934e8c
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2026-08-24 15:23:51 +0700

    add (Main.py): addding paddleOCR and pre-processing image

--- main.py
+++ main.py
@@ -2,12 +2,81 @@ from fastapi import FastAPI, Depends, HTTPException, status, UploadFile, File
 from PIL import Image
 import io
 from ultralytics import YOLO
+from paddleocr import PaddleOCR
+import numpy as np 
+import cv2
 
 app = FastAPI()
 model = YOLO(r"D:\Coding\Web\Back-End\plat_fast\yolov11s_fold3.pt")
+ocr_engine = PaddleOCR(use_angle_cls=True, lang='en', enable_mkldnn=False)
 
-def ocr(image):
-    return "BL123H"
+def preprocess_image(image_pil):
+
+    img_np = np.array(image_pil.convert("RGB"))
+    img_bgr = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+
+    scale = 4
+    img_upscaled = cv2.resize(
+        img_bgr,
+        None,
+        fx=scale,
+        fy=scale,
+        interpolation=cv2.INTER_CUBIC
+    )
+
+    # Tambahkan padding
+    img_padded = cv2.copyMakeBorder(
+        img_upscaled,
+        top=20,
+        bottom=20,
+        left=20,
+        right=20,
+        borderType=cv2.BORDER_CONSTANT,
+        value=[255, 255, 255]
+    )
+
+    cv2.imwrite("debug_paddle.jpg", img_padded)
+
+    return img_padded
+
+def ocr(image_pil):
+    try:
+        img_siap_ocr = preprocess_image(image_pil)
+
+        result = ocr_engine.ocr(img_siap_ocr)
+
+        if not result or len(result) == 0:
+            return "Plat tidak terbaca (Result Kosong)"
+        
+        data_ocr = result[0]
+        
+        if not data_ocr:
+            return "Plat tidak terbaca (Elemen pertama kosong)"
+
+        teks_gabungan = ""
+        
+
+        if isinstance(data_ocr, dict) and 'rec_texts' in data_ocr:
+            list_teks = data_ocr['rec_texts'] 
+            
+            teks_mentah = "".join(list_teks)
+            
+            teks_bersih = ''.join(char for char in teks_mentah if char.isalnum()).upper()
+            teks_gabungan = teks_bersih
+            
+        else:
+             return f"Struktur data OCR tidak dikenali: Tipe {type(data_ocr)}"
+
+        if len(teks_gabungan) < 4:
+            return "Terbaca namun keliru: " + teks_gabungan
+        
+        return teks_gabungan
+        
+    except Exception as e:
+        import traceback
+        print("--- ERROR PADDLEOCR ---")
+        traceback.print_exc() 
+        return f"Error OCR: {e}"
 
 @app.post("/plat")
 async def deteksi_plat(file: UploadFile = File(...)):
